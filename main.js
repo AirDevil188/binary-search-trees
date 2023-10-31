d8ef4eae2683e54e5a356df616c6155a2825a4fc
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-10-31 23:21:24 +0100

    added package.json

---
+++ main.js
@@ -0,0 +1,4 @@
+import { Tree } from "./modules/tree.js";
+const tree = new Tree();
+// console.log(tree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
+console.log(tree.test([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
---
+++ modules/tree.js
@@ -0,0 +1,3 @@
+/// <root> <left> <right> Pre Order
+// <left> <root> <right> In Order
+// <left> <right> <root> Post Order
