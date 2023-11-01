592cada2286b4b2bc42ae103c9cf0ffa64197b6a
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2023-11-01 12:12:23 +0100

    added sort and started working on my buildTree method, in my tree.js module

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import { Tree } from "./modules/tree.js";
+import Tree from "./modules/tree.js";
 const tree = new Tree();
-// console.log(tree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
-console.log(tree.test([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
+console.log(tree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
--- modules/tree.js
+++ modules/tree.js
@@ -1,3 +1,31 @@
+export default class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  sort(a, b) {
+    return a - b;
+  }
+
+  buildTree(array) {
+    let sortedArray = [...new Set(array)].sort(this.sort);
+    return sortedArray;
+  }
+
+  prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+}
+
 /// <root> <left> <right> Pre Order
 // <left> <root> <right> In Order
 // <left> <right> <root> Post Order
