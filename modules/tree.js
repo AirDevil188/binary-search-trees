5176ae61a1ca3610f9be5dc5ce78c3dae86074d1
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2023-11-18 12:36:16 +0100

    fixed the insert method, and delete method

--- modules/tree.js
+++ modules/tree.js
@@ -24,11 +24,7 @@ export default class Tree {
       return;
     }
     if (node.right !== null) {
-      this.prettyPrint(
-        node.right,
-        `${prefix}${isLeft ? "│   " : "    "}`,
-        false
-      );
+      this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
     if (node.left !== null) {
@@ -38,72 +34,135 @@ export default class Tree {
 
   insert(value, root = this.root) {
     let newNode = new Node(value);
-    if (root === null) {
+    if (!root) {
       this.root = newNode;
       return root;
     }
-    if (value < root) {
-      if (root.left === null) root.left = newNode;
+    if (value < root.data) {
+      if (!root.left) root.left = newNode;
       else this.insert(value, root.left);
     } else {
-      if (root.right === null) root.right = newNode;
+      if (!root.right) root.right = newNode;
       else this.insert(value, root.right);
     }
-    return null;
+    return;
   }
 
-  delete(value, root = this.root, prev = null) {
-    // check if bst is empty if it is return
-    if (root === null) return;
-    // if there is only one value (root) assign it  tu null
-    if (root.left === null && root.right === null && prev === null)
-      return (root.data = null);
+  delete(value, root = this.root, parent = null) {
+    let temp = root;
+    if (!root) return root;
+
+    // traverse left
     if (value < root.data) {
-      // delete leaf nodes
-      if (root.data === value && root.right === null && root.left === null) {
-        if (prev.left !== null && prev.left.data === value) prev.left = null;
-        else prev.right = null;
-
-        // delete nodes with one child
-      } else if (
-        root.data === value &&
-        root.left !== null &&
-        root.right === null
-      ) {
-        root.data = root.left.data;
-        root.left = null;
-      } else if (
-        root.data === value &&
-        root.right !== null &&
-        root.left === null
-      ) {
-        root.data = root.right.data;
-        root.right = null;
-      } else this.delete(value, root.left, root);
+      if (root.left) this.delete(value, root.left, root);
+      // traverse right
     } else {
-      // delete leaf nodes
-      if (root.data === value && root.right === null && root.left === null) {
-        if (prev.right !== null && prev.right.data === value) prev.right = null;
-        else prev.left = null;
-
-        // delete nodes with one child
-      } else if (
-        root.data === value &&
-        root.right !== null &&
-        root.left === null
-      ) {
-        root.data = root.right.data;
-        root.right = null;
-      } else if (
-        root.data === value &&
-        root.left !== null &&
-        root.right === null
-      ) {
-        root.data = root.left.data;
-        root.left = null;
-      } else this.delete(value, root.right, root);
+      if (root.right) this.delete(value, root.right, root);
     }
-    return;
+    // root node case
+    if (!parent && root.data === value && (!root.right || !root.left)) {
+      if (root.right) {
+        parent = root.right;
+        this.root = null;
+        this.root = parent;
+      } else {
+        parent = root.left;
+        this.root = null;
+        this.root = parent;
+      }
+      return;
+    }
+
+    // delete leaf nodes
+    if (root.data === value && !root.left && !root.right) {
+      if (parent.left && parent.left.data === value) parent.left = null;
+      else parent.right = null;
+    }
+
+    // delete nodes with 1 child
+
+    if (root.right && !root.left && root.data === value) {
+      // if it only has 1 right child
+
+      if (!root.right.right) {
+        root.data = root.right.data;
+        if (!root.right.left) {
+          root.right = null;
+        } else if (root.left.right && parent.left.data === value) {
+          parent.left = parent.left.right;
+        }
+      } else if (!parent.right && !parent.left) {
+        parent.right = root.right;
+        // if it has parent on the right
+      } else if (parent.right) {
+        if (parent.left) {
+          parent.right = root.right;
+        } else {
+          parent.right = root.left;
+        }
+      }
+    } else if (root.left && !root.right && !root.left.left && root.data === value) {
+      if (root.left && !root.right && root.data === value) {
+        // if it only has 1 left child
+        if (!root.left.left) {
+          if (!root.left.right && !root.right) {
+            root.data = root.left.data;
+            root.left = null;
+          } else if (root.left.right && parent.right.data === value) {
+            root.data = root.left.data;
+            parent.right = parent.right.left;
+          } else if (root.left.right && parent.left.data === value) {
+            root.data = root.left.data;
+            parent.left = root.left;
+          }
+          // if it has 1 left child with grandchildren
+        } else if (!parent.left && !parent.right) {
+          parent.left = root.left;
+          // if it has parent on the left
+        } else if (parent.left) {
+          parent.right = root.left;
+        }
+      }
+    } // delete nodes with two children
+    else if (root.data === value && root.right && root.left) {
+      if (temp.right.right) temp = temp.right;
+      else {
+        temp = root;
+        parent = temp;
+        temp = temp.right;
+      }
+
+      while (temp.left) {
+        parent = temp;
+        temp = temp.left;
+      }
+
+      if (temp.right && !temp.left) {
+        if (!temp.right.right) {
+          root.data = temp.data;
+          temp.data = temp.right.data;
+
+          temp.right = null;
+        } else {
+          root.data = temp.data;
+          root.right = temp.right;
+        }
+      } else if (temp.left && !temp.right) {
+        parent.data = temp.left.data;
+        temp.left = null;
+      } else if (!temp.right && !temp.left) {
+        if (parent.right) {
+          root.data = temp.data;
+          if (parent.right.data === temp.data) parent.right = null;
+          else if ((parent.left.data = temp.data)) parent.left = null;
+        } else if (parent.left) {
+          root.data = temp.data;
+          if (parent.left.data === temp.data) parent.left = null;
+          else if (parent.right.data === temp.data) parent.right = null;
+        }
+      }
+    }
+    return root;
   }
 }
 
